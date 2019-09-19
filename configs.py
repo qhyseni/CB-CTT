eecc0f2af5f38cadb4d5b119a93c0d1e9c85f90b
qhyseni <qendresehyseni@gmail.com>
2019-09-19 20:57:20 +0100

    tl experiment

--- configs.py
+++ configs.py
@@ -3,10 +3,10 @@ class configs:
 
     # 1 - XML; 2 - ECTT
     instance_type = 2
-    cbctt_dir = '/home/qhyseni/Documents/Thesis/cb-ctt/'
-    datasets_dir = '/home/qhyseni/Documents/Thesis/datasets/'
-    jar_path = '/home/qhyseni/Documents/Thesis/cb-ctt/cb-ctt-new.jar'
-    sbps_path = '/home/qhyseni/Documents/Thesis/cb-ctt/loandra_static'
+    cbctt_dir = '/home/fiek/Documents/Thesis/cb-ctt/'
+    datasets_dir = '/home/fiek/Documents/Thesis/datasets/'
+    jar_path = '/home/fiek/Documents/Thesis/cb-ctt/cb-ctt-new.jar'
+    sbps_path = '/home/fiek/Documents/Thesis/cb-ctt/loandra_static'
     instance_name = 'comp01.ectt'
     output_name = 'output.txt'
     maxsat_timeout = '5000'
---
+++ experiments_TL.py
@@ -0,0 +1,55 @@
+import xlwt
+from datetime import datetime
+from configs import configs
+from parameters import parameters
+from alns import alns
+import random
+
+import time
+instances = [
+            'comp01', 'comp02',
+            'comp03', 'comp04', 'comp05',
+             'comp06', 'comp07', 'comp08', 'comp09','comp10'
+             # 'comp11', 'comp12','comp13', 'comp14', 'comp15',
+             # 'comp16', 'comp17', 'comp18', 'comp19', 'comp20','comp21'
+]
+
+
+TL = [50, 100, 250, 500, 1000, 2000, 4000, 6000, 8000, 10000]
+
+# ------------ w1 parameter tuning ------------- #
+
+configs.instance_name = "comp02.ectt"
+
+wb = xlwt.Workbook()
+
+ws = wb.add_sheet('TL_' + configs.instance_name)
+
+ws.write(0, 0, "TL")
+ws.write(0, 1, "Cost")
+ws.write(0, 2, "Time")
+
+row = 0
+for param_value in TL:
+
+    print("\nTL: ", param_value)
+
+    parameters.time_limit = param_value
+
+    print('INSTANCE' + configs.instance_name +'\n')
+
+    for i in range(0, 5):
+        row = row + 1
+        print(i)
+        alns_instance = alns()
+        start_time = time.time()
+        schedule, cost = alns_instance.execute()
+        end_time = time.time()
+        exec_time = end_time - start_time
+
+        ws.write(row, 0, param_value)
+        ws.write(row, 1, cost)
+        ws.write(row, 2, exec_time)
+
+        wb.save('experiments_TL' + configs.instance_name + '.xls')
+
